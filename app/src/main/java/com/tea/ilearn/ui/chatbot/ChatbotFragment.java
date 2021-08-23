6e545d3d62c15b325da2551ba01eb4f702381d92
acha <323163497@qq.com>
2021-08-23 14:25:35 +0800

    hide keyboard after send

--- app/src/main/java/com/tea/ilearn/ui/chatbot/ChatbotFragment.java
+++ app/src/main/java/com/tea/ilearn/ui/chatbot/ChatbotFragment.java
@@ -1,11 +1,16 @@
 package com.tea.ilearn.ui.chatbot;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -44,10 +49,20 @@ public class ChatbotFragment extends Fragment {
         sendText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mMessageAdapter.add(new Message(editText.getText().toString(), 0));
-                mMessageAdapter.add(new Message("小艾在读幼儿园，啥也不会呢", 1));
-                editText.setText("");
-                // TODO : hide keyboard here
+                String msg = editText.getText().toString();
+                if (msg.length() == 0) {
+                    Toast toast = Toast.makeText(getContext(), "请输入后再发送", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                }
+                else {
+                    mMessageAdapter.add(new Message(msg, 0)); // user send a msg
+                    mMessageAdapter.add(new Message("小艾在读幼儿园，啥也不会呢", 1)); // TODO eduKG api
+                    editText.setText("");
+                    mMessageRecycler.scrollToPosition(mMessageAdapter.getItemCount()-1);
+                }
+                InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
             }
         });
     }
