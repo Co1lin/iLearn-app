6f8a34250a5bbeebc8d8303a3378834770517b19
Co1lin <mail@co1in.me>
2021-08-29 01:45:19 +0800

    init top bar #13

---
+++ actionbarex-common/src/main/java/per/goweii/actionbarex/common/ActionIconView.java
@@ -0,0 +1,27 @@
+package per.goweii.actionbarex.common;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public final class ActionIconView extends AppCompatImageView {
+
+    public ActionIconView(Context context) {
+        this(context, null);
+    }
+
+    public ActionIconView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ActionIconView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(heightSize, heightSize);
+    }
+}
