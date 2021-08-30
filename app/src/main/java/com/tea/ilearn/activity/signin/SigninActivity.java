31857c5342d8c682291bd8bfb5e8bdcbf5d16f53
acha <323163497@qq.com>
2021-08-30 23:35:50 +0800

    signup ui

--- app/src/main/java/com/tea/ilearn/activity/signin/SigninActivity.java
+++ app/src/main/java/com/tea/ilearn/activity/signin/SigninActivity.java
@@ -1,10 +1,12 @@
 package com.tea.ilearn.activity.signin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.tea.ilearn.activity.signup.SignupActivity;
 import com.tea.ilearn.databinding.ActivitySigninBinding;
 
 public class SigninActivity extends AppCompatActivity {
@@ -19,7 +21,7 @@ public class SigninActivity extends AppCompatActivity {
 
         // TODO validation error base on database (below are examples)
         /*
-        binding.usernameBox.setError("用户名不存在");
+        binding.usernameBox.setError("用户名不存在"); // or 邮箱未注册
         binding.passwordBox.setError("密码错误");
         binding.usernameBox.setError(null);
         binding.passwordBox.setError(null);
@@ -30,7 +32,8 @@ public class SigninActivity extends AppCompatActivity {
         });
 
         binding.tosignup.setOnClickListener($ -> {
-            // TODO
+            Intent intent = new Intent(root.getContext(), SignupActivity.class);
+            root.getContext().startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/tea/ilearn/activity/signup/SignupActivity.java
+++ app/src/main/java/com/tea/ilearn/activity/signup/SignupActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.florent37.viewtooltip.ViewTooltip;
 import com.tea.ilearn.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
@@ -17,5 +18,22 @@ public class SignupActivity extends AppCompatActivity {
         View root = binding.getRoot();
         setContentView(root);
 
+        // TODO validator
+        binding.tosignin.setOnClickListener($ -> {
+            finish(); // TODO clear previous signin activity info ?
+        });
+        binding.signup.setOnClickListener($ -> {
+            if (!binding.agree.isChecked()) {
+                ViewTooltip.on(binding.agree)
+                    .autoHide(true, 1000)
+                    .corner(30)
+                    .position(ViewTooltip.Position.TOP)
+                    .text("请先勾选同意")
+                    .show();
+            }
+            // TODO database
+        });
+        binding.agree.setOnCheckedChangeListener((btn, checked) -> {
+        });
     }
 }
