bece6cc19119ce2f6304bbc60c2a609ae34288af
acha <323163497@qq.com>
2021-09-02 00:04:49 +0800

    refactor echart to mpaandroidchart

--- app/src/main/java/com/tea/ilearn/ui/me/MeFragment.java
+++ app/src/main/java/com/tea/ilearn/ui/me/MeFragment.java
@@ -2,24 +2,30 @@ package com.tea.ilearn.ui.me;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.github.abel533.echarts.axis.CategoryAxis;
-import com.github.abel533.echarts.axis.ValueAxis;
-import com.github.abel533.echarts.code.X;
-import com.github.abel533.echarts.json.GsonOption;
-import com.github.abel533.echarts.series.Bar;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.tea.ilearn.R;
 import com.tea.ilearn.activity.account.SigninActivity;
 import com.tea.ilearn.databinding.FragmentMeBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MeFragment extends Fragment {
     private FragmentMeBinding binding;
@@ -42,37 +48,49 @@ public class MeFragment extends Fragment {
             }
         });
 
-        binding.barChart.setWebViewClient(new WebViewClient(){
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                refreshBarChart();
-            }
+        // =======================================================================
 
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
+        LineChart lineChart = binding.lineChart;
+        List<String> key = Arrays.asList("周一", "周二", "周三", "周四", "周五", "周六", "周日");
+        List<Integer> value = Arrays.asList(120, 200, 150, 80, 10, 110, 130); // TODO real value here
+        List<Entry> entries = new ArrayList<Entry>();
+        for (int i = 0; i < key.size(); ++i) {
+            entries.add(new Entry(i, value.get(i)));
+        }
+
+        LineDataSet dataset = new LineDataSet(entries, "本周学习点"); // add entries to dataset
+        TypedValue typedValue = new TypedValue();
+        binding.getRoot().getContext().getTheme().resolveAttribute(R.attr.colorOnPrimary, typedValue, true);
+        int color = ContextCompat.getColor(binding.getRoot().getContext(), typedValue.resourceId);
+        dataset.setValueTextSize(10);
+        dataset.setValueTextColor(color);
+        dataset.setDrawFilled(true);
+        dataset.setFillDrawable(ContextCompat.getDrawable(root.getContext(), R.drawable.gradient_fill));
+        lineChart.setClipValuesToContent(false);
+        lineChart.getXAxis().setTextColor(color);
+        typedValue = new TypedValue();
+        binding.getRoot().getContext().getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        color = ContextCompat.getColor(binding.getRoot().getContext(), typedValue.resourceId);
+        dataset.setColor(color);
+
+        LineData lineData = new LineData(dataset);
+        lineChart.setDragEnabled(false);
+        lineChart.setScaleEnabled(false);
+        lineChart.setScaleXEnabled(false);
+        lineChart.setScaleYEnabled(false);
+        lineChart.setPinchZoom(false);
+        lineChart.getDescription().setEnabled(false);
+        lineChart.getLegend().setEnabled(false);
+        lineChart.getAxisRight().setEnabled(false);
+        lineChart.getAxisLeft().setEnabled(false);
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        lineChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(key));
+        lineChart.getXAxis().setDrawGridLines(false);
+        lineChart.getAxisLeft().setDrawGridLines(false);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
 
         return root;
-    }
-
-
-    private void refreshBarChart() {
-        GsonOption option = new GsonOption();
-        option.xAxis(new CategoryAxis().data("周一", "周二", "周三", "周四", "周五", "周六", "周日"));
-        option.yAxis(new ValueAxis());
-        option.title().text("本周学习点").x(X.center);
-
-        Bar bar = new Bar();
-        bar.itemStyle().normal().label().show(true).position("inside");
-        bar.data(120, 200, 150, 80, 70, 110, 130);
-
-        option.series(bar);
-
-        binding.barChart.refreshEchartsWithOption(option);
     }
 }
 // TODO left padding of the text
