408a2103f61e799d8190fdbe70320ce1b103fe20
acha <323163497@qq.com>
2021-09-11 21:18:20 +0800

    add exam icon

--- app/src/main/java/com/tea/ilearn/MyApplication.java
+++ app/src/main/java/com/tea/ilearn/MyApplication.java
@@ -2,6 +2,8 @@ package com.tea.ilearn;
 
 import android.app.Application;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.tea.ilearn.net.APIRequest;
 import com.tea.ilearn.utils.ObjectBox;
 
@@ -11,5 +13,8 @@ public class MyApplication extends Application {
         super.onCreate();
         ObjectBox.init(this);
         APIRequest.setContext(getApplicationContext());
+
+        // TODO colin
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 }
